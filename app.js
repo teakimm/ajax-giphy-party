89302c994d52b67349ffcf4c234da213a50a7a3d
Thomas Kim <tkimm156@gmail.com>
2024-02-13 20:01:27 -0600

    Refactor code

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
 "use strict";
 
-/** This retrieves our search word and calls the giphy API, to grab gif of our
- * search word--> appends it to our html.
+const API_KEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
+const GIPHY_URL = "http://api.giphy.com/v1/gifs/"
+
+/** This retrieves our url and calls the giphy API, to grab the gif url
+ * and returns it
   */
-//  TODO: input this later
-const APIKEY = 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym';
-//TODO: make a conductor -- decomposition on getGif
 
 async function getGif() {
-
   const searchTerm = $("#search-term").val();
-  const url = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`;
+  const params = new URLSearchParams({searchTerm});
+  const url = `${GIPHY_URL}search?q=${params}&api_key=${API_KEY}`;
   const response = await fetch(url);
   const gifData = await response.json();
-  const gifUrl = gifData.data[0].images.original.url;
-  // TODO: random url index
+  //Giphy gives us 50 gifs, choose one randomly
+  const gifUrl = gifData.data[getRandomNumber(0, 50)].images.original.url;
 
-  makeGif(gifUrl, searchTerm);
+  return gifUrl;
 }
 
-//not calling it here because javascript is calling this async
-$(".submit").on("click", getGif);
+/** return a random number between min and max*/
+function getRandomNumber(min, max) {
+  return Math.ceil(Math.random() * (max - min) + min);
+}
 
 /** Adding image to the webpage, and appending to our gif container section */
 
-function makeGif(url, searchTerm) {
-
+function prependGif(url) {
   const gifImage = $("<img>", {
     src: url,
-    class: 'gif',
-    alt: `this is a gif of ${searchTerm}`
+    class: 'gif'
   });
 
-  $('.gif-container').append(gifImage);
+  $('.gif-container').prepend(gifImage);
 }
+
+/** controller function gets form data, calls giphy api and adds gif to
+ * DOM.
+ */
+async function getAndShowGif() {
+  const gifUrl = await getGif();
+  prependGif(gifUrl);
+}
+
+/** calls getAndShowGif when button is clicked */
+
+$(".submit").on("click", getAndShowGif);
 
 /** Event listener-- listening for clicks to remove all gifs in gif container */
 

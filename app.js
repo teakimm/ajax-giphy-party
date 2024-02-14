390b4eb88006dd0663b70b7b8a805ff367ac776d
Thomas Kim <tkimm156@gmail.com>
2024-02-13 18:10:40 -0600

    Establish connection with giphy api

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+"use strict";
 console.log("Let's get this party started!");
+
+async function getGif() {
+  const searchTerm = $("#search-term").val();
+  const url = `http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`;
+  const response = await fetch(url);
+  const gifData = await response.json();
+  console.log("url is:", gifData.data[0].images.original.url);
+}
+
+$(".submit").on("click", getGif);
